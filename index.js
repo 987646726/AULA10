cffe76087571f32c6f5275c37a21dcbce5a34aab
987646726 <christiangomes2809@gmail.com>
2025-05-13 22:01:18 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let produto = prompt (`Digite o preço de um produto`)
+
+let cor = prompt (`Digite a cor da etiqueta`)
+
+let desconto = 0
+
+if (cor === 'Vermelho') {
+    desconto 
+} else if (cor === 'Vermelho') {
+    console.log(`Desconto de 10`)
+}
